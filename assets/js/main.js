d497688824fe092e354ca7b7945f69d36f084faa
yusufginanjar <52599899+yusufginanjar@users.noreply.github.com>
2019-11-03 21:16:46 +0700

    rev 1
    add scroll page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,17 @@
-$(function(){
-	//cache the window object
-	var $window = $(window);
-	//parallax background effect
-	$('section[data-type="background"]').each(function(){
-		var $bgobj = $(this);
-		$window.scroll(function(){
-			//scroll the background at var speed
-			//the yPos is a nagative value because we're scrollin up
-			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
-			//put together our final background position
-			var coords = '50%' + yPos + 'px';
-			//move the background
-			$bgobj.css({backgroundPosition: coords});
-		}); //end window scroll
-	});
 
-});
+// Page Scroll to Register
+$(document).ready(function(){
+	$('.page-scroll').on('click', function(e){
+	  var tujuan = $(this).attr('href');
+	  var elemenTujuan = $(tujuan);
+	  
+  
+	  $("html, body").animate({
+		  scrollTop: elemenTujuan.offset().top - 30
+		}, 800, function(){
+	  });
+  
+	  e.preventDefault();
+  });
+  });
+  
